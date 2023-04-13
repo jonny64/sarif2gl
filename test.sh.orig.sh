c5049f94baaefb4713b21c1127ada167926bae72
jonny64 <mgruzdev@cheby.ru>
2023-04-13 17:10:21 +0300

    Update test.sh.orig.sh

--- test.sh.orig.sh
+++ test.sh.orig.sh
@@ -1,7 +1,10 @@
-export CI_JOB_TOKEN=
-export CI_MERGE_REQUEST_ID=42
-export CI_SERVER_URL=https://gitlab.com
-export CI_PROJECT_PATH="mg/test"
+export SDL_BOT_TOKEN=
+export CI_MERGE_REQUEST_IID=42
+export CI_SERVER_URL="https://some.company.org"
+export CI_MERGE_REQUEST_PROJECT_URL="https://some.company.org/namespace/project"
+export CI_PROJECT_DIR="/builds/namespace/project"
+export CI_PROJECT_PATH="namespace/project"
 export CI_COMMIT_SHA=
 export CI_MERGE_REQUEST_DIFF_BASE_SHA=
+export CI_PIPELINE_URL="https://some.company.org/namespace/project/-/pipelines/4242"
 node sarif2gl.js semgrep.sarif
